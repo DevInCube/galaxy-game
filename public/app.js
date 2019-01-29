4f5c40c922c07616aff1f266e898425ecd7ca820
phadyniak <public.hadyniak@gmail.com>
2019-01-30 00:53:52 +0200

    add multiple planets & time pause

--- public/app.js
+++ public/app.js
@@ -11,8 +11,13 @@ System.register("import-example", [], function (exports_1, context_1) {
     };
 });
 System.register("main", ["pixi.js", "import-example"], function (exports_2, context_2) {
-    var PIXI, import_example_1, width, height, app, stage, g, ball;
+    var PIXI, import_example_1, app, stage, mode, width, height, g, ball, pls, t;
     var __moduleName = context_2 && context_2.id;
+    function onKeyDown(key) {
+        if (key.keyCode === 32) {
+            mode = mode === 0 ? 1 : 0;
+        }
+    }
     // Resize function window
     function resize() {
         // Get the p
@@ -22,33 +27,58 @@ System.register("main", ["pixi.js", "import-example"], function (exports_2, cont
         // You can use the 'screen' property as the renderer visible
         // area, this is more useful than view.width/height because
         // it handles resolution
-        g.position.set(app.screen.width, app.screen.height);
+        //g.position.set(app.screen.width, app.screen.height);
+        width = app.screen.width;
+        height = app.screen.height;
+    }
+    function createPl() {
+        var distance = Math.random() * 250 + 50;
+        var speed = Math.random() - 0.5;
+        var pl = {
+            position: {
+                x: 0,
+                y: 0
+            },
+            distance: distance,
+            speed: speed + (speed / Math.abs(speed)) * 0.3,
+            startAngle: Math.random() * 2 * Math.PI,
+            sprite: (function () {
+                var g = new PIXI.Graphics();
+                g.beginFill(0x00ffff);
+                g.lineStyle(0);
+                var size = Math.random() * 15 + 5;
+                g.drawCircle(size, size, size);
+                g.endFill();
+                var sprite = new PIXI.Sprite(g.generateCanvasTexture());
+                sprite.anchor.set(.5, .5);
+                stage.addChild(sprite);
+                return sprite;
+            })()
+        };
+        return pl;
     }
     function update() {
-        ball.velocity.y += .1; // gravity
-        ball.position.x += ball.velocity.x;
-        ball.position.y += ball.velocity.y;
-        if (ball.position.x < 0) {
-            ball.position.x = -ball.position.x;
-            ball.velocity.x = -ball.velocity.x;
+        ball.position.x = width / 2;
+        ball.position.y = height / 2;
+        for (var _i = 0, pls_1 = pls; _i < pls_1.length; _i++) {
+            var pl = pls_1[_i];
+            pl.position.x = ball.position.x + pl.distance * Math.cos(pl.startAngle + pl.speed * t / 1000);
+            pl.position.y = ball.position.y + pl.distance * Math.sin(pl.startAngle + pl.speed * t / 1000);
         }
-        if (ball.position.y < 0) {
-            ball.position.y = -ball.position.y;
-            ball.velocity.y = -ball.velocity.y;
-        }
-        if (ball.position.x >= width) {
-            ball.position.x = width - ball.position.x + width;
-            ball.velocity.x = -ball.velocity.x;
-        }
-        if (ball.position.y >= width) {
-            ball.position.y = height - ball.position.y + height;
-            ball.velocity.y = -ball.velocity.y;
+        if (mode === 1) {
+            // global tiem
+            t += 10;
         }
         setTimeout(update, 10); // 100 UPS
     }
     function render(timestamp) {
         ball.sprite.x = ball.position.x;
         ball.sprite.y = ball.position.y;
+        for (var _i = 0, pls_2 = pls; _i < pls_2.length; _i++) {
+            var pl = pls_2[_i];
+            pl.sprite.x = pl.position.x;
+            pl.sprite.y = pl.position.y;
+        }
         app.renderer.render(stage);
         requestAnimationFrame(render);
     }
@@ -63,26 +93,29 @@ System.register("main", ["pixi.js", "import-example"], function (exports_2, cont
         ],
         execute: function () {
             console.log("import " + import_example_1.ok());
-            width = 256;
-            height = 256;
             app = new PIXI.Application({
                 autoResize: true,
                 resolution: devicePixelRatio
             });
             document.querySelector('#frame').appendChild(app.view);
             stage = new PIXI.Container();
-            g = new PIXI.Graphics();
+            mode = 1;
+            // Add the 'keydown' event listener to our document
+            document.addEventListener('keydown', onKeyDown);
             // Listen for window resize events
             window.addEventListener('resize', resize);
+            width = 256;
+            height = 256;
             resize();
+            g = new PIXI.Graphics();
+            g.beginFill(0xffff00);
+            g.lineStyle(0);
+            g.drawCircle(10, 10, 10);
+            g.endFill();
             ball = {
-                velocity: {
-                    x: 5,
-                    y: 3
-                },
                 position: {
-                    x: 100,
-                    y: 200
+                    x: 0,
+                    y: 0
                 },
                 sprite: (function () {
                     var sprite = new PIXI.Sprite(g.generateCanvasTexture());
@@ -91,6 +124,11 @@ System.register("main", ["pixi.js", "import-example"], function (exports_2, cont
                     return sprite;
                 })()
             };
+            pls = [];
+            for (var i = 0; i < 5; i++) {
+                pls.push(createPl());
+            }
+            t = 0;
             setTimeout(update);
             requestAnimationFrame(render);
         }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import * as PIXI from "pixi.js";
 import { ok } from "./import-example";
 console.log(`import ${ok()}`);
 
-const width = 256;
-const height = 256;
-
 const app = new PIXI.Application({
     autoResize: true,
     resolution: devicePixelRatio,
@@ -14,10 +11,22 @@ document.querySelector('#frame').appendChild(app.view);
 
 const stage = new PIXI.Container();
 
-const g = new PIXI.Graphics();
+let mode = 1;
+
+// Add the 'keydown' event listener to our document
+document.addEventListener('keydown', onKeyDown);
+
+function onKeyDown(key: KeyboardEvent) {
+    if (key.keyCode === 32) {
+        mode = mode === 0 ? 1 : 0;
+    }
+}
 
 // Listen for window resize events
 window.addEventListener('resize', resize);
+
+let width = 256;
+let height = 256;
 
 // Resize function window
 function resize() {
@@ -31,18 +40,23 @@ function resize() {
     // You can use the 'screen' property as the renderer visible
     // area, this is more useful than view.width/height because
     // it handles resolution
-    g.position.set(app.screen.width, app.screen.height);
+    //g.position.set(app.screen.width, app.screen.height);
+    width = app.screen.width;
+    height = app.screen.height;
 }
 resize();
 
+
+const g = new PIXI.Graphics();
+g.beginFill(0xffff00);
+g.lineStyle(0);
+g.drawCircle(10, 10, 10);
+g.endFill();
+
 const ball = {
-    velocity: {
-        x: 5,
-        y: 3,
-    },
     position: {
-        x: 100,
-        y: 200
+        x: 0,
+        y: 0,
     },
     sprite: (() => {
         const sprite = new PIXI.Sprite(g.generateCanvasTexture());
@@ -52,27 +66,52 @@ const ball = {
     })()
 }
 
+const pls: any[] = [];
+for (let i = 0; i < 5; i++) {
+    pls.push(createPl());
+}
 
+function createPl() {
+    const distance = Math.random() * 250 + 50;
+    const speed = Math.random() - 0.5;
+    const pl = {
+        position: {
+            x: 0,
+            y: 0
+        },
+        distance: distance,
+        speed: speed + (speed / Math.abs(speed)) * 0.3,
+        startAngle: Math.random() * 2 * Math.PI,
+        sprite: (() => {
+            const g = new PIXI.Graphics();
+            g.beginFill(0x00ffff);
+            g.lineStyle(0);
+            const size = Math.random() * 15 + 5;
+            g.drawCircle(size, size, size);
+            g.endFill();
+            const sprite = new PIXI.Sprite(g.generateCanvasTexture());
+            sprite.anchor.set(.5, .5);
+            stage.addChild(sprite);
+            return sprite;
+        })()
+    }
+    return pl;
+}
+
+let t = 0;
 
 function update() {
-    ball.velocity.y += .1; // gravity
-    ball.position.x += ball.velocity.x;
-    ball.position.y += ball.velocity.y;
-    if (ball.position.x < 0) {
-        ball.position.x = -ball.position.x;
-        ball.velocity.x = -ball.velocity.x;
+    ball.position.x = width / 2;
+    ball.position.y = height / 2;
+
+    for (const pl of pls) {
+        pl.position.x = ball.position.x + pl.distance * Math.cos(pl.startAngle + pl.speed * t / 1000);
+        pl.position.y = ball.position.y + pl.distance * Math.sin(pl.startAngle + pl.speed * t / 1000);
     }
-    if (ball.position.y < 0) {
-        ball.position.y = -ball.position.y;
-        ball.velocity.y = -ball.velocity.y;
-    }
-    if (ball.position.x >= width) {
-        ball.position.x = width - ball.position.x + width;
-        ball.velocity.x = -ball.velocity.x;
-    }
-    if (ball.position.y >= width) {
-        ball.position.y = height - ball.position.y + height;
-        ball.velocity.y = -ball.velocity.y;
+
+    if (mode === 1) {
+        // global tiem
+        t += 10;
     }
 
     setTimeout(update, 10); // 100 UPS
@@ -81,6 +120,11 @@ function update() {
 function render(timestamp: number) {
     ball.sprite.x = ball.position.x;
     ball.sprite.y = ball.position.y;
+
+    for (const pl of pls) {
+        pl.sprite.x = pl.position.x;
+        pl.sprite.y = pl.position.y;
+    }
 
     app.renderer.render(stage);
     requestAnimationFrame(render);
